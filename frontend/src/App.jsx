a7431560f78fb0e8b73adf160113a1da30bf89a6
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-06-19 17:55:22 +0530

    update state varibales and usestate variable

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+import Sidebar from "./Sidebar.jsx";
+import Chatwindow from "./Chatwindow.jsx";
+import { MyContext } from "./MyContext.jsx";
+import { useState } from "react";
+import {v1 as uuid} from "uuid";
+
+
+function App() {
+  const [prompt , setPrompt] = useState("");
+  const [reply , setReply] = useState(null);
+  const [currThreadId , setcurrThreadId] = useState(uuid());
+  const [prevChats , setPrevChats] = useState([]);  //stores all chats of curr thread
+  const [newChat , setNewChat] = useState(true);
+  
+
+ const providerValues = {
+            prompt , setPrompt , 
+            reply , setReply,
+            currThreadId , setcurrThreadId,
+            newChat , setNewChat,
+            prevChats , setPrevChats
+
+ };
+
+  return (
+    <div className='app'>
+    
+    <MyContext.Provider value = {providerValues}>
+       <Sidebar> </Sidebar>
+       <Chatwindow> </Chatwindow>
+    </MyContext.Provider> 
+    </div>
+  )
+}
+
+export default App;
