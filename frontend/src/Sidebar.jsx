42548d58d66fb3344a3c2e17700fe330b977cf3a
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-06-21 12:39:20 +0530

    filterad all the api response to show only thread title

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -1,26 +1,58 @@
 import "./Sidebar.css";
+import { useContext, useEffect } from "react";
+import { MyContext } from "./MyContext";
+import {v1 as uuidv1} from "uuid";
+
 function Sidebar(){
+  const { allThreads, setAllThreads, currThreadId ,setNewChat , setPrompt , setReply , setCurrThreadId , setPrevChtas} = useContext(MyContext);
+
+  const getAllThreads = async () => {
+    try {
+        const response = await fetch("http://localhost:8080/api/chat/threads");
+        const res = await response.json();
+
+        const filterdData = res.map(thread => ({ threadId : thread.threadId , title : thread.title}))
+        console.log(filterdData);
+        setAllThreads(filterdData);
+
+        // thread id , title
+
+       
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getAllThreads();
+  }, []);
+
+      const createNewChat = ()=>{
+        setNewChat(true);
+        setPrompt(" ");
+        setReply(null);
+        setCurrThreadId(uuidv1());
+        setPrevChats([]);
+      }
+
     return (
          <section className="sidebar">
 
-          {/* new chat button*/}
-        <button>
+        <button onClick={createNewChat}>
              <img src="src/assets/blacklogo.png" alt="gptlogo" className="logo"></img>
               <span>
                    <i className="fa-solid fa-pen-to-square"></i>
               </span>
         </button>
 
-          {/* history*/}
           <ul className="history">
-            <li> history 1</li>
-            <li> history 1</li>
-            <li> history 1</li>
+            {
+              allThreads?.map((thread, idx)=>(
+                <li key={idx}>{thread.title}</li>
+              ))
+            }
           </ul>
 
-
-          {/* sign*/}
-          
           <div className="sign">
             <p>by Vaishnavi Mahajan &hearts;</p>
           </div>
