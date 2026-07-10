17183b54024950821eca9e895ecceb5214978dd8
vaishnaviimahajann <vaishnavimahajann@gmail.com>
2026-07-10 10:32:33 +0530

    Fix logo image path for production build

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -2,6 +2,7 @@ import "./Sidebar.css";
 import { useContext, useEffect } from "react";
 import { MyContext } from "./MyContext";
 import {v1 as uuidv1} from "uuid";
+import logo from "./assets/blacklogo.png";
 
 function Sidebar(){
   const { allThreads, setAllThreads, currThreadId, setNewChat, setPrompt, setReply, setcurrThreadId, setPrevChats } = useContext(MyContext);
@@ -67,7 +68,7 @@ function Sidebar(){
   return (
     <section className="sidebar">
       <button onClick={createNewChat}>
-        <img src="src/assets/blacklogo.png" alt="gptlogo" className="logo"></img>
+        <img src={logo} alt="gptlogo" className="logo"></img>
         <span>
           <i className="fa-solid fa-pen-to-square"></i>
         </span>
